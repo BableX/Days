6c0bafa9d7b93bb27358f8aa92f6b6e75ca4702f
MARSEL ATTOKUROV <blackdeyzir@gmail.com>
2025-10-27 11:30:42 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let text = prompt (`Напиши какой сегодня день недели!`)
+
+switch (text) {
+    case (`1`):
+        alert(`Понедельник`)
+        break;
+
+    case (`2`):
+        alert(`Вторник`)
+        break;
+
+           case (`3`):
+        alert(`Среда`)
+        break;
+
+           case (`4`):
+        alert(`Четверг`)
+        break;
+
+           case (`5`):
+        alert(`Пятница`)
+        break;
+
+           case (`6`):
+        alert(`Суббота`)
+        break;
+
+           case (`7`):
+        alert(`Восскресенье`)
+        break;
+    default:
+        alert(`Error 404`)
+        break;
+}
